22226202cedfa86329ed2d58961c3c3052d77fcd
AleksandraSudareva <37781212+AleksandraSudareva@users.noreply.github.com>
2018-04-04 23:48:15 +0300

    Update generate.py
    I wrap the work with the dictionary and compose the text into the class MakeText

--- generate.py
+++ generate.py
@@ -31,43 +31,54 @@ length = int(args.len)
 output = args.out
 
 
-def make_text():    # генерируем свой текст на основе словаря
-    list_of_keys = list()
-    for i in Dictionary.keys():
-        list_of_keys.append(i)
-    if seed != '0':
-        begining_word = args.seed
-    else:
-        begining_word = random.choice(list_of_keys)
-    result = list()
-    result.append(begining_word)
-    len_of_text = args.len
-    for i in range(0, int(len_of_text)):
-        sum = 0
-        list_of_prob = list()
-        for j in Dictionary[result[i]].values():
-            sum += int(j)
-        for j in Dictionary[result[i]].values():
-            list_of_prob.append(int(j)/sum)
-        list_of_values = list()
-        for j in Dictionary[result[i]].keys():
-            list_of_values.append(j)
-        new_word = random.choices(list_of_values, weights=list_of_prob, k=1)
-        result.append(new_word[0])
-    return result
+class MakeText:
+    def OpenModel(self):
+        model = args.mo
+        with open(model, 'rb') as k:    # загружаем словарь из указанного файла
+            Dictionary = pickle.load(k)
+        return Dictionary
 
 
-model = args.mo
-with open(model, 'rb') as k:    # загружаем словарь из указанного файла
-    Dictionary = pickle.load(k)
+    def make_text(self, Dictionary):
+    # генерируем свой текст на основе словаря
+        list_of_keys = []
+        for i in Dictionary.keys():
+            list_of_keys.append(i)
+        if seed != '0':
+            begining_word = args.seed
+        else:
+            begining_word = random.choice(list_of_keys)
+        result = []
+        result.append(begining_word)
+        len_of_text = args.len
+        for i in range(0, int(len_of_text)):
+            sum = 0
+            list_of_prob = []
+            for j in Dictionary[result[i]].values():
+                sum += int(j)
+            for j in Dictionary[result[i]].values():
+                list_of_prob.append(int(j) / sum)
+            list_of_values = []
+            for j in Dictionary[result[i]].keys():
+                list_of_values.append(j)
+            new_word = random.choices(list_of_values, weights=list_of_prob, k=1)
+            result.append(new_word[0])
+        return result
 
-# выводим созданный нами текст в заданный файл(иначе в stdout)
-if args.out != 'stdout':
-    path = args.out
-    file = glob.glob(path)
-    with open(file) as f:
-        text = make_text()
-        f.write(text)
-else:
-    text = make_text()
-    print(' '.join(text))
+
+    def Output(self, Dictionary):
+        # выводим созданный  нами текст в заданный файл(иначе в stdout)
+        if args.out != 'stdout':
+            path = args.out
+            file = glob.glob(path)
+            with open(file) as f:
+                text = self.make_text(Dictionary)
+                f.write(text)
+        else:
+            text = self.make_text(Dictionary)
+            print(' '.join(text))
+
+
+x = MakeText()
+Dict = x.OpenModel()
+x.Output(Dict)
